1f1f75f4d8eb956d54d9ab2e1c6a34694832b315
Royal-13 <gunjanbaunthiyal17@gmail.com>
2022-09-21 14:35:40 +0530

    initial

---
+++ script.js
@@ -0,0 +1,35 @@
+const DOB = document.querySelector("#dob");
+const LuckyNumber = document.querySelector("#lucky-number");
+const CheckBtn = document.querySelector("#check");
+const Output = document.querySelector("#output");
+
+function calculateSum(d) {
+    d = d.replaceAll("-","");
+    let sum = 0;
+    for(let i=0;i<d.length;i++)
+    {
+        sum += Number(d.charAt(i));
+    }
+    return sum;
+}
+
+function compareValues(sum,luck) {
+    if(sum % luck === 0 ) {
+        Output.innerText = "Your birthday is lucky ";
+    } else {
+        Output.innerText = "Your birthday is not lucky ";
+    }
+}
+
+function CheckNumber() {
+    const d = DOB.value;
+    const sum = calculateSum(d);
+    if(sum && d) {
+        compareValues(sum,LuckyNumber.value);
+    } else {
+        Output.innerText = "Please enter both the fields.. ";
+    }
+    
+}
+
+CheckBtn.addEventListener('click', CheckNumber);
